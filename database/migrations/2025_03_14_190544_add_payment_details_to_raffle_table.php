3a30a81278f131df3fa968f46b97168f4599ccbb
Fabio <fabiorvs@outlook.com>
2025-05-25 21:10:36 -0300

    Fix Migrate

--- database/migrations/2025_03_14_190544_add_payment_details_to_raffle_table.php
+++ database/migrations/2025_03_14_190544_add_payment_details_to_raffle_table.php
@@ -6,14 +6,14 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up()
     {
-        Schema::table('rifas', function (Blueprint $table) {
+        Schema::table('raffles', function (Blueprint $table) {
             $table->text('payment_details')->nullable()->after('status');
         });
     }
 
     public function down()
     {
-        Schema::table('rifas', function (Blueprint $table) {
+        Schema::table('raffles', function (Blueprint $table) {
             $table->dropColumn('payment_details');
         });
     }
